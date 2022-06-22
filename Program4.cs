db6477658c18eef44bd67497ac3a2b30435d36df
Voldemar-imp <108013210+Voldemar-imp@users.noreply.github.com>
2022-06-22 21:01:16 +0300

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string age;
+            string zodiacSign;
+            string jobPlace;
+
+            Console.Write("Введите Ваше имя: ");
+                name = Console.ReadLine();
+            Console.Write("Введите Ваш возраст: ");
+                age = Console.ReadLine ();
+            Console.Write("Введите Ваш знак зодиака: ");
+                zodiacSign = Console.ReadLine ();
+            Console.Write("Введите Ваше место работы: ");
+                jobPlace = Console.ReadLine ();
+
+            Console.WriteLine ("Вас зовут {0}, вам {1} год, вы {2} и работаете на {3}.", 
+                name, age, zodiacSign, jobPlace);
+        }
+    }
+}
